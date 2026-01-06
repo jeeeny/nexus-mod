1e55f21907312128752f58a1e6ae0a4e385b73d7
十一 <shiyi@shiyideMacBook-Pro.local>
2026-01-06 17:58:07 +0800

    修复页面问题及图片

--- script.js
+++ script.js
@@ -245,7 +245,7 @@ if (searchInput) {
       (m.local_name_en && m.local_name_en.toLowerCase().includes(query)) ||
       (m.name_cn && m.name_cn.toLowerCase().includes(query)) ||
       ((m.mod_id + "").includes(query)) ||
-      (m.summary && m.summary.toLowerCase().includes(query)) ||
+      (m.summary_cn && m.summary_cn.toLowerCase().includes(query)) ||
       (m.author && m.author.toLowerCase().includes(query)) ||
       parseTags(m.tags).some(tag => tag.toLowerCase().includes(query))
     );
